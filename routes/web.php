5798caf8ee5b378b3974f763f3519563c54e7d1c
boumedyen <boumedyen121@gmail.com>
2025-05-01 18:52:44 +0100

    Add redis for fun

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,17 @@
 <?php
 
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
 
+Route::get('/cache/testing', function () {
 
+    if (Cache::store('redis')->put('redis-test', 'Redis is working!', 100)) {
+        return Cache::store('redis')->get('redis-test');
+    }
+
+    return 'Redis connection failed';
+});
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
